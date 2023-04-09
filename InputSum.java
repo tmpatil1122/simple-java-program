94f3d4d0cb867b460f9955f7d9872f262450343e
Tushar <tmpatil0406@gmail.com>
2023-04-09 20:52:54 +0530

    Adding files

---
+++ InputSum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class InputSum {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int sum = A + B;
+        System.out.println("Sum of two number is= " + sum);
+    }
+}
