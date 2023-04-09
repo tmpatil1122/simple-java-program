54b8461190c393024a62d9873e4931720f037be7
Tushar <tmpatil0406@gmail.com>
2023-04-09 20:44:16 +0530

    nitial commit

---
+++ Input.java
@@ -0,0 +1,15 @@
+
+//simple inputs using java
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+        int number = sc.nextInt();
+        System.out.println(number);
+        float price = sc.nextFloat();
+        System.out.println(price);
+    }
+}
