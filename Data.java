2a433ec3ffc065eef776cfac7b0fc3872c5c29f5
Tushar <tmpatil0406@gmail.com>
2023-04-09 20:09:44 +0530

    nitial commit

--- Data.java
+++ Data.java
@@ -1,3 +1,20 @@
+//simple Data Types in Java
 public class Data {
+    public static void main(String args[]) {
+        byte b = 7;
+        System.out.println(b);
+        char ch = 't';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        float price = 7.5f;
+        System.out.println(price);
+        int number = 10;
+        System.out.println(number);
+        // long
+        // double
+        short n = 240;
+        System.out.println(n);
+    }
 
 }
