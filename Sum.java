d92829923156628e062952e39d6414e9ede15347
Tushar <tmpatil0406@gmail.com>
2023-04-09 20:24:18 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+//simple sum of two numbers
+public class Sum {
+    public static void main(String args[]) {
+        int a = 15;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+}
