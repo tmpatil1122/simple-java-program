648421803dfe3f6872acfe4a4da59a908f6f0004
Tushar <tmpatil0406@gmail.com>
2023-04-10 19:06:33 +0530

    initial commit

---
+++ Product.java
@@ -0,0 +1,13 @@
+
+//Simple code for product of two numbers
+import java.util.*;
+
+public class Product {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println("The Product of two number is= " + product);
+    }
+}
