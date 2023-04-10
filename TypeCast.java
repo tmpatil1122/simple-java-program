403272741aa902feb93e5bcc69771658afc89838
Tushar <tmpatil0406@gmail.com>
2023-04-10 19:36:43 +0530

    initial commit

---
+++ TypeCast.java
@@ -0,0 +1,8 @@
+//Simple program of Type Casting
+public class TypeCast {
+    public static void main(String args[]) {
+        float marks = 99.99f;
+        int marks2 = (int) marks;
+        System.out.println(marks2);
+    }
+}
