9c21705ffab2b75f083714e7b034945cda871878
Tushar <tmpatil0406@gmail.com>
2023-04-10 19:17:24 +0530

    initial commit

---
+++ AreaCircle.java
@@ -0,0 +1,12 @@
+
+//Code to calculate area of circle
+import java.util.*;
+
+public class AreaCircle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+        float area = 3.14f * radius * radius;
+        System.out.println("Area of circle= " + area);
+    }
+}
