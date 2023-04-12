4de2ba40e6ab3bf697888fb7c101a667b96f5b4a
Tushar <tmpatil0406@gmail.com>
2023-04-12 18:24:09 +0530

    initial commit

---
+++ Areasquare.java
@@ -0,0 +1,12 @@
+
+//Simple java program to get area of square
+import java.util.*;
+
+public class Areasquare {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        float side = sc.nextFloat();
+        float area = side * side;
+        System.out.println(area);
+    }
+}
